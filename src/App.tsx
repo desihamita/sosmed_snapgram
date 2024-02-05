dbeb41e7304cf9e6e72b1b60bd85e06fc2f5a2c1
desihamita <69859667+desihamita@users.noreply.github.com>
2024-02-05 11:22:10 +0700

    HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
+import { Toaster } from "@/components/ui/toaster"
 
 import './globals.css';
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages/Index';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
-import { Toaster } from "@/components/ui/toaster"
+
+import { Home } from './_root/pages/Index';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+
+import AllUsers from './_root/pages/AllUsers';
+import Explore from './_root/pages/Explore';
+import CreatePost from './_root/pages/CreatePost';
+import Profile from './_root/pages/Profile';
+import PostDetails from './_root/pages/PostDetails';
+import EditPost from './_root/pages/EditPost';
+import Saved from './_root/pages/Saved';
+import UpdateProfile from './_root/pages/UpdateProfile';
 
 const App = () => {
   return (
@@ -21,6 +31,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout/>}>
           <Route index element={<Home/>} />
+          <Route path="/explore" element={<Explore/>} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost/>} />
+          <Route path="/update-post/:id" element={<EditPost/>} />
+          <Route path="/posts/:id" element={<PostDetails/>} />
+          <Route path="/profile/:id/*" element={<Profile/>} />
+          <Route path="/update-profile/:id" element={<UpdateProfile/>} />
         </Route>
       </Routes>
       
