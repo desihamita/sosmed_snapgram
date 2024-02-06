bc0abf5fe9e446071520b951bc0e160b80058a93
desihamita <69859667+desihamita@users.noreply.github.com>
2024-02-06 13:56:38 +0700

    Create Post

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,11 +1,13 @@
-import { INewUser } from "@/types";
+import { INewPost, INewUser } from "@/types";
 import { 
     useQuery,
     useMutation,
     useQueryClient,
-    useInfiniteQuery
+    useInfiniteQuery,
+    useQueries
 } from "@tanstack/react-query";
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
+import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from "../appwrite/api";
+import { QUERY_KEYS } from "./queryKeys";
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -24,4 +26,23 @@ export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount
     })
+}
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (post: INewPost) => createPost(post),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+      },
+    });
+};
+
+export const useGetRecentPosts = () => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryFn: () => getRecentPosts(),
+    });
 }
